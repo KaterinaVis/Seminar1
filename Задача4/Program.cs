ab827f7bb0d31e82496f7150efb4ecafaa5b9afb
Katerina <katerina4484@mail.ru>
2022-08-08 21:57:53 +0500

    задача44

--- Задача4/Program.cs
+++ Задача4/Program.cs
@@ -1,4 +1,11 @@
 ﻿//  Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
-Console.WriteLine("Введите трехзначное число");
+Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(number % 10);
+if(number > 99)
+{
+if(number < 1000)
+{
+int lastDigit = number % 10;
+Console.WriteLine(lastDigit);
+}
+}
